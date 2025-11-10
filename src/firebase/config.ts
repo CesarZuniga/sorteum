aee8741b1127ad28a621a63f684e46855956d415
Cesar Uriel Zuñiga Salazar <cesaruriel007@gmail.com>
2025-11-10 19:29:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3864058285-bb036",
+  "appId": "1:473664070843:web:dcddc09c2b6cc36aed359e",
+  "apiKey": "AIzaSyC9jox3P8dXgFGxAaMczb-ZerTOY5TCfBk",
+  "authDomain": "studio-3864058285-bb036.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "473664070843"
+};
